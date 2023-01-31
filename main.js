50dfdb9ba7ca2c273c3e40d96a932531ef1ffae8
Nolyanne <117562981+Nolyanne@users.noreply.github.com>
2023-01-31 09:47:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,50 @@
-
+var num1=["0"];
+var num2=["0"];
+var sinal=["vazio"];
+var result=0;
 
 function numClic(clic){
-
-    if (num1==='0'){
-            num1=clic;
-            document.getElementsByClassName('resultado').innerHTML = num1;
-    } else if (sinal==='vazio'){
+    if (num1=='0'){
+        num1=clic;
+        document.write('resultado') = num1;
+    } else if (sinal=='vazio'){
             num1=num1+clic;
-            document.getElementsByClassName('resultado').innerHTML = num1;
-        } else if (num2==='0'){
-            num2=clic;
-            document.getElementsByClassName('resultado').innerHTML = num2;
+            document.write('resultado') = num1;
+        } else if (num2=='0'){
+                num2=clic;
+                document.write('resultado') = num2;
         } else {
             num2=num2+clic;
-            document.getElementsByClassName('resultado').innerHTML = num2;
+            document.write('resultado') = num2;
         }
     
-
     } 
     
 function opClic(op){
     sinal = op;
-
 } 
 
 
 function limpar(){
-    document.getElementsByClassName('resultado').innerHTML = "";
+    num1="0";
+    num2="0";
+    sinal="vazio";
+    result=0;
+    document.write('resultado') = "0";
 }
 
-function calc(num1, num2, sinal){
-    
-    if (sinal==='mais'){
-        result = parseint(num1) + parseint(num2);
-    } else if (sinal==='menos'){
-        result = parseint(num1) - parseint(num2);
-    } else if (sinal==='mult'){
-        result = parseint(num1) * parseint(num2);        
-    } else if (sinal==='div'){
-        result = parseint(num1) / parseint(num2);    
-    } else{
-        result = '0';
-        document.getElementsByClassName('resultado').innerHTML = result;
+function calc(){
+    const numm1 = parseInt(num1);
+    const numm2 = parseInt(num2);
+    if (sinal=='mais'){
+        result = numm1 + numm2;
+    } else if (sinal=='menos'){
+        result = numm1 - numm2;
+    } else if (sinal=='mult'){
+        result = numm1 * numm2;        
+    } else if (sinal=='div'){
+        result = numm1 / numm2;    
+    } 
+    document.write('resultado') = result;
+    limpar();
     }
-     
-
-}
